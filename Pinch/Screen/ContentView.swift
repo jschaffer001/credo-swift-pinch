7269914bd605bbb621067d99f2d9ede5c33d336a
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-15 09:58:19 -0700

    Added new info panel view that implements a long touch gesture to toggle the view on and off. Still need to work on getting the ultraThinMaterial background material to work right now it's not visable as I would like it to be.

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -28,6 +28,8 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
+                
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -73,6 +75,14 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top
+            )
+            
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
