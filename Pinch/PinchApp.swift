8515e3e28ad1714c9077547fe84cf15be07bf0fa
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-14 21:13:49 -0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jonathan Schaffer on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
